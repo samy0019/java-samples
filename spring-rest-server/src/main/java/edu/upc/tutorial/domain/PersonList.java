a6522bebe70661d874a707a96c3e5b4b9f28a9f2
Jose Diaz <jose.diaz@joedayz.pe>
2012-02-07 20:45:02 -0500

    Anadiendo PersonList

--- spring-rest-server/src/main/java/edu/upc/tutorial/domain/PersonList.java
+++ spring-rest-server/src/main/java/edu/upc/tutorial/domain/PersonList.java
@@ -1,5 +1,21 @@
 package edu.upc.tutorial.domain;
 
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+
+@XmlRootElement(name="persons")
 public class PersonList {
 
+	private List<Person> data;
+
+	public List<Person> getData() {
+		return data;
+	}
+
+	public void setData(List<Person> data) {
+		this.data = data;
+	}
 }
