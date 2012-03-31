c4b90bcf00c058d55d7fa804577c852464c480df
Jose Diaz <jose.diaz@joedayz.pe>
2012-03-31 18:48:05 -0500

    Spring REST Client

---
+++ spring-rest-client/src/main/java/org/krams/tutorial/domain/PersonList.java
@@ -0,0 +1,19 @@
+package org.krams.tutorial.domain;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name="persons")
+public class PersonList {
+
+	private List<Person> data;
+
+	public List<Person> getData() {
+		return data;
+	}
+
+	public void setData(List<Person> data) {
+		this.data = data;
+	}
+}
