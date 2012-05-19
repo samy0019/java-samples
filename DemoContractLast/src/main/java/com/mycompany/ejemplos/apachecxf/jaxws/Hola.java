8a836a986837f6668f02c4d512007687a76c045b
Jose Diaz <jose.diaz@joedayz.pe>
2012-05-19 11:27:30 -0500

    interface webservice

---
+++ DemoContractLast/src/main/java/com/mycompany/ejemplos/apachecxf/jaxws/Hola.java
@@ -0,0 +1,15 @@
+package com.mycompany.ejemplos.apachecxf.jaxws;
+
+
+public interface Hola {
+
+	
+	/**
+	 * Operaci—n del Web Service
+	 * 
+	 * @param nombre
+	 * @return String con un saludo
+	 */
+	public String saludar( String nombre);
+	
+}
