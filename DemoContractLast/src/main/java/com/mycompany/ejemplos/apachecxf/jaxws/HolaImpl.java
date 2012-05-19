a33638618ac09c33e2f984b8aa4e75146348fd7f
Jose Diaz <jose.diaz@joedayz.pe>
2012-05-19 11:46:52 -0500

    implementacion

---
+++ DemoContractLast/src/main/java/com/mycompany/ejemplos/apachecxf/jaxws/HolaImpl.java
@@ -0,0 +1,22 @@
+package com.mycompany.ejemplos.apachecxf.jaxws;
+
+import javax.jws.WebService;
+
+
+/**
+ * Implementaci—n del Web Service, el endpointInterface es el fully qualified
+ * name de la interface que implementa.
+ * 
+ * @author joedayz
+ */
+
+
+@SuppressWarnings("restriction")
+@WebService(endpointInterface = "com.mycompany.ejemplos.apachecxf.jaxws.Hola")
+public class HolaImpl implements Hola {
+
+	public String saludar(String nombre) {
+		return "Hola " + nombre;
+	}
+
+}
