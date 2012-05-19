fd98f9f75b518cabb0722bd3a675c19848b623bd
Jose Diaz <jose.diaz@joedayz.pe>
2012-05-19 11:39:21 -0500

    Interface lista

--- DemoContractLast/src/main/java/com/mycompany/ejemplos/apachecxf/jaxws/Hola.java
+++ DemoContractLast/src/main/java/com/mycompany/ejemplos/apachecxf/jaxws/Hola.java
@@ -1,15 +1,24 @@
-package com.mycompany.ejemplos.apachecxf.jaxws;
+package net.lshimokawa.ejemplos.apachecxf.jaxws;
 
+import javax.jws.WebParam;
+import javax.jws.WebService;
 
-public interface Hola {
+/**
+ * Interface del WebService SOAP. Utiliza las anotaciones de JAX-WS. La
+ * configuraci—n de Apache CXF se encuentra en el applicationContext.xml dentro
+ * del WEB-INF.
+ * 
+ * @author lshimokawa
+ */
+@WebService
+public interface HelloWorld {
 
-	
 	/**
 	 * Operaci—n del Web Service
 	 * 
 	 * @param nombre
 	 * @return String con un saludo
 	 */
-	public String saludar( String nombre);
-	
+	public String saludar(@WebParam(name = "nombre") String nombre);
+
 }
