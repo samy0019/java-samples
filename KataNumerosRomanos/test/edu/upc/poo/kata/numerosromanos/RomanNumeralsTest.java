46f66c95c2b3eea9f37bf3c34a64af7ceccfe2a7
Jose Diaz <jose.diaz@joedayz.pe>
2012-05-20 18:52:46 -0500

    Kata Numeros Romanos

---
+++ KataNumerosRomanos/test/edu/upc/poo/kata/numerosromanos/RomanNumeralsTest.java
@@ -0,0 +1,22 @@
+package edu.upc.poo.kata.numerosromanos;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class RomanNumeralsTest {
+
+	RomanNumerals romNum = new RomanNumerals();
+
+	@Test
+	public void testTotal() {
+		for (int i = 1; i <= 3000; i++) {
+
+			String roman = romNum.encode(i);
+
+			assertEquals(i, romNum.decode(roman));
+			assertEquals(roman, romNum.encode(i));
+
+		}
+	}
+}
